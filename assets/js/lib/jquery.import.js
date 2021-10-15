57d9267249b6fa601a3a1ae98d0aa5e214872495
김가영 Kim gayoung <77196388+GAYOUNG914@users.noreply.github.com>
2021-10-15 17:56:17 +0900

    ㅇㄴ

--- assets/js/lib/jquery.import.js
+++ assets/js/lib/jquery.import.js
@@ -1,17 +1,23 @@
-(function() {
+(function () {
     let codeview = new Array();
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/jquery-1.11.1.min.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/jquery.nice-select.min.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/jquery-ui.min.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/jquery.ui.touch-punch.min.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/countUp.umd.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/jquery.mCustomScrollbar.concat.min.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/swiper.min.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/jquery.drawsvg.min.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/lib/aos.js', 'cashbuster': false });
-    codeview.push({ 'url': '/distHyundai/assets/js/ui.common.js', 'cashbuster': true });
+    codeview.push({ url: "/distHyundai/assets/js/lib/jquery-1.11.1.min.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/lib/jquery.nice-select.min.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/lib/jquery-ui.min.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/lib/jquery.ui.touch-punch.min.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/lib/countUp.umd.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/lib/jquery.mCustomScrollbar.concat.min.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/lib/swiper.min.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/lib/jquery.drawsvg.min.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/lib/aos.js", cashbuster: false });
+    codeview.push({ url: "/distHyundai/assets/js/ui.common.js", cashbuster: true });
 
     for (let a = 0, atotal = codeview.length; a < atotal; a++) {
-        document.write('<script src="' + codeview[a].url + ((codeview[a].cashbuster) ? '?cb=' + new Date().getTime() : '') + '" charset="utf-8"></' + 'script>');
-    };
+        document.write(
+            '<script src="' +
+                codeview[a].url +
+                (codeview[a].cashbuster ? "?cb=" + new Date().getTime() : "") +
+                '" charset="utf-8"></' +
+                "script>"
+        );
+    }
 })();
